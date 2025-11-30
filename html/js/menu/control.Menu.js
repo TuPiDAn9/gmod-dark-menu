a836c1e7ded915b20c9cd543b10a2d19c6f63e7b
TuPiDAn <83866462+TuPiDAn9@users.noreply.github.com>
2025-11-30 14:15:23 +0800

    bumv version

--- html/js/menu/control.Menu.js
+++ html/js/menu/control.Menu.js
@@ -12,7 +12,7 @@ var MapIndex = {}
 	setTimeout(function(){
 	console.log( "THANK YOU for using Dark Main Menu mod" );
 	console.log( "Visit https://github.com/TuPiDAn9/gmod-dark-menu for updates and support." );
-	console.log( "Made by TuPiDAn | v1.3" );
+	console.log( "Made by TuPiDAn | v1.3.3" );
 }, 5000);
 
 var subscriptions = new Subscriptions();
